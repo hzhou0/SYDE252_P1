94e95620eb842581f29cc4c9a6b17e7d23911fa0
CynicalBread <31447440+CynicalBread@users.noreply.github.com>
2022-11-21 16:27:51 -0500

    Added a factor of -1 to the median window size
    Median filter was not taking the correct upper bound when indexing signal array for a given window size

--- median_av.m
+++ median_av.m
@@ -1,16 +1,19 @@
 function y = median_av(x, window_size)
     i=1;
     y=1:numel(x);
+    
     % Take a sliding window over the signal
     for sig=1:numel(x)
        lower_bound=i-window_size;
+       
        % If lower bound is out of bounds, set it as 1
        % For smaller window sizes (<1000) this is irrelevant
        if lower_bound<1
            lower_bound=1;
        end
+       
        % Set output as the median of the sliding window
-       y(i)=median(x(lower_bound:lower_bound+window_size,:));
+       y(i)=median(x(lower_bound:lower_bound+window_size-1,:));
        i=i+1; 
     end
     y=y';
